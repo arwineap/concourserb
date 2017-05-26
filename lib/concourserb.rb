a3b20c9b743cf6b5d09726cc4a3ecbbb10a01887
Alex Arwine <alex.arwine@thrivemarket.com>
2017-05-26 11:35:15 -0700

    removing debugging lines
    These lines were debugging a scenario where we were no longer authed
    but did not receive a 401 as part of that auth.
    
    This issue was resolved once we started using the Authorization header
    instead of the ATC-Authorization cookie.
    
    Concourse behaves much better to this gem now that it considers us an
    api client instead of a CSRF lockdown

--- lib/concourserb.rb
+++ lib/concourserb.rb
@@ -76,11 +76,7 @@ class Concourserb
         request['Content-Type'] = 'application/json'
         request['Authorization'] = "Bearer #{@ATC_auth}"
         response = http.request(request)
-        if response.code == 401 or response.body.eql?('not authorized')
-            # debug
-            if response.code != 401
-                puts "refresh_token response.code: #{response.code}"
-            end
+        if response.code == 401
             # we got bad auth, so get a new token
             auth()
             request['Authorization'] = "Bearer #{@ATC_auth}"
